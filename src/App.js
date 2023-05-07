8fd7df03d743fe9c9d2db2b867e94f8543e02762
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-05-08 01:22:33 +0200

    Initial
    Fetches products and displays them.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Menu from './Components/Dishes/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product List</h1>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
